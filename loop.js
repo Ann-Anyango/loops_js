325ae83d755c3830b8413ed5c722a24f34f7b3e7
Ann-Anyango <annmwangombe@gmail.com>
2023-04-22 17:39:53 +0300

    first commit

---
+++ loop.js
@@ -0,0 +1,39 @@
+// for(var i=0;i<=10;i++){
+//     console.log(i)
+// }
+let number=0
+for (var num=1;num<=100;num++){
+   number=number + num;
+
+}
+console.log("The numbers are:"+ num);
+
+let Fruies=["orange","Mangoes","banana","berry"]
+for(i=0;i<Fruies.length;i++){
+    console.log(Fruies[i]);
+}
+let data={
+    name:"Ann",
+    age:20,
+    Gender:"Female", 
+
+}
+for (var elemne in data){
+    console.log(elemne,data[elemne])
+}
+   
+// hosting
+console.log(c);
+// using var it allows you 
+var  c=50;
+console.log({c});
+greet();
+function greet(){
+    console.log("Hello");
+    
+}
+
+
+
+
+
